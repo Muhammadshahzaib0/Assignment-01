0b791840a00045739062d3887c12dab0aed6dde5
shahzaib <shahzaibxmarty02@gmail.com>
2023-04-02 15:36:29 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,60 @@
+alert("This my work");
+
+console.log("Ramzan Mubarak");
+
+
+var title ="Ramzan or javascript";
+console.log( title);
+
+
+var a = 30;
+var b = 50; 
+var c = 100;
+var x =(a + b) - c
+console.log(x);
+
+
+var x ="MUHAMMMAD";
+var y ="SHAHZAIB";
+
+
+var z = x + y;
+console.log(z)
+
+// **************poss incriment*****************
+
+var a =50;
+a++;
+console.log(a)
+
+
+var b =100;
+b++;
+console.log(b)
+
+// **************poss dicriment*****************
+
+var a =80;
+a--;
+console.log(a)
+
+
+var b =100;
+b--;
+console.log(b)
+
+
+var firstname = prompt("enter your name","Muhammad");
+var lastname = prompt("enter your lastname", "Shahzaib");
+var city = prompt("enter city", "Karachi");
+console.log(firstname + " " + lastname + " From " + city);
+
+
+var correct = true;
+
+if ( correct == true) {
+    console.log("yes");
+} else{
+    console.log("no");
+}
+
